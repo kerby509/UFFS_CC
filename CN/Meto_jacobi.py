22981a445ac186450cdeb118e4acec71efa09189
kerby <lovincekerby99@gmail.com>
2022-06-15 10:46:34 -0300

    commit

--- CN/Meto_jacobi.py
+++ CN/Meto_jacobi.py
@@ -10,3 +10,15 @@ def comparar(x,xk,eps):
         return True
   else:
      return False  
+ 
+ 
+def gaussJacobi(A,b,maxiter,eps):
+     n = len(b)
+     sol = True
+     x = b.copy()
+     for i in list(range(1,n+1,1)):
+        if (math.fabs(A[i-1][i-1]) > 0.0):
+            x[i-1] = b[i-1]/A[i-1][i-1]
+        else:
+            sol = False
+        break
