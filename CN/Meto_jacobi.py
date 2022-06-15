e84d4aa67438d93e7bbe01a7e112e636e068d361
kerby <lovincekerby99@gmail.com>
2022-06-15 10:52:31 -0300

    Gauss-Jacobi

--- CN/Meto_jacobi.py
+++ CN/Meto_jacobi.py
@@ -39,3 +39,22 @@ def gaussJacobi(A,b,maxiter,eps):
                          s = s + A[i-1][j-1]*x[j-1]
 
                          xk[i-1] = (1/A[i-1][i-1])*(b[i-1]-s)
+                         
+                         print("Iteração: ",iter)
+                         print("xk = ",xk)
+                         if comparar(x,xk,eps):
+                             x = xk.copy()
+                             break    
+                         x = xk.copy()
+     
+   
+            return x
+        #10,2,1,1,5,1,2,3,10,,7,-8,6
+A = [[10, 2,  1],
+     [1, 5,  1],
+     [ 2,3, 10]]
+#aqui colocamos a igualdade da eq
+b = [7, -8, 6]
+
+x = gaussJacobi(A,b,1,2)
+print("x = ",x)
