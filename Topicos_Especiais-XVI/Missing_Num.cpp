74f6cad624037e401b9e1d37ef994d7ab045c75d
kerby <lovincekerby99@gmail.com>
2022-10-06 14:31:57 -0300

    add

--- Topicos_Especiais-XVI/Missing_Num.cpp
+++ Topicos_Especiais-XVI/Missing_Num.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 using namespace std;
+
+int main (){
+
+    long long  n,num, sum =0;
+    cin >> n;
+
+    for (int i = 0; i<n-1; i++) {
+        cin >> num;
+        sum+= num;
+    }
+
+    cout << (n * (n + 1)) / 2 - sum<<  endl;
+
+    return 0 ;
+
+}
+
+
+
