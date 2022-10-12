ac68aad84eb359ea3be77c8348915285948bc3a1
kerby <lovincekerby99@gmail.com>
2022-10-12 19:20:12 -0300

    add

---
+++ Topicos_Especiais-XVI/TwoKnights.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main(){
+	ll n; cin>>n;
+	for(int i=1;i<=n;i++){
+		//Total number of cell in chessboard
+    //Número total de células no tabuleiro
+		ll t=i*i;
