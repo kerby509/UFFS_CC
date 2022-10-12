4067db787284a2b63a39dfd8e445ab50a32f64ad
kerby <lovincekerby99@gmail.com>
2022-10-12 19:20:45 -0300

    add

--- Topicos_Especiais-XVI/TwoKnights.cpp
+++ Topicos_Especiais-XVI/TwoKnights.cpp
@@ -9,3 +9,15 @@ int main(){
 		//Total number of cell in chessboard
     //Número total de células no tabuleiro
 		ll t=i*i;
+        //Total number of combination of two places
+    //Número total de combinação de dois lugares
+		ll k=t*(t-1)/2;
+		//Número total de possibilidades que 2 cavaleiros podem atacar um ao outro
+		if(i>2) k-=4*(i-1)*(i-2);
+		//total de lugares será k
+		cout<<k<<"\n";
+	}
+ 
+	return 0;
+}
+ 
