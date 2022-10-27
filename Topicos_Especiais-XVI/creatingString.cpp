c62986ef55f48a5b1dfa8d1ffcfd226c13eaf989
kerby <lovincekerby99@gmail.com>
2022-10-27 12:30:23 -0300

    add

--- Topicos_Especiais-XVI/creatingString.cpp
+++ Topicos_Especiais-XVI/creatingString.cpp
@@ -5,7 +5,10 @@ void permute(string str, int l, int r, set<string> &lks){
     if(l==r) lks.insert(str);
     else{
         for(int i = l; i<= r; i++){
-            
+            swap(str[l], str[i]);
+            permute(str, l+1, r , lks);
+            swap(str[l], str[i]);
+
         }
     }
 }
