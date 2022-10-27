e444511bee0f0a332b7fade033e2ec50978c32ba
kerby <lovincekerby99@gmail.com>
2022-10-27 12:28:42 -0300

    add

--- Topicos_Especiais-XVI/creatingString.cpp
+++ Topicos_Especiais-XVI/creatingString.cpp
@@ -1,2 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+void permute(string str, int l, int r, set<string> &lks){
+    if(l==r) lks.insert(str);
+    else{
+        for(int i = l; i<= r; i++){
+            
+        }
+    }
+}
