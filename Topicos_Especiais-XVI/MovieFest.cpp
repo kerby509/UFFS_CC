0f2cd7f1c5ff04e533fdf014b66d5ca0e21a834f
kerby <lovincekerby99@gmail.com>
2022-11-10 13:42:12 -0300

    add

--- Topicos_Especiais-XVI/MovieFest.cpp
+++ Topicos_Especiais-XVI/MovieFest.cpp
@@ -16,4 +16,18 @@ int main(){
           // pushing end time, start time
           v.push_back({c,a});
        }
+
+       sort(v.begin(),v.end());
+
+       ll curr=v[0].first;
+       ll ans=1;
+       for(ll i=1;i<n;i++){
+           if(v[i].second>=curr){
+            // take this
+            curr=v[i].first;
+            ans++;
+           }
+       }
+       cout<<ans<<"\n";
+    }
 }
