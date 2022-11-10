5cf6f47755017277c0fc14a3a19886aaf7207ef5
kerby <lovincekerby99@gmail.com>
2022-11-10 13:33:15 -0300

    add

--- Topicos_Especiais-XVI/RestCustomer.cpp
+++ Topicos_Especiais-XVI/RestCustomer.cpp
@@ -1,2 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<pair<int, int>> times;
+	for (int i = 0; i < n; i++) {
+		int start, end;
+		cin >> start >> end;
+		times.push_back({start, 1});
+		times.push_back({end, -1});
+	}
