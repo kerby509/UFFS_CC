402715b8bc1a9fd7384e3970600218e2e765aac7
kerby <lovincekerby99@gmail.com>
2022-11-10 13:41:31 -0300

    add

--- Topicos_Especiais-XVI/MovieFest.cpp
+++ Topicos_Especiais-XVI/MovieFest.cpp
@@ -9,4 +9,11 @@ int main(){
     while(T--){
        vector<pair<ll,ll>> v;
        ll n;
+       cin>>n;
+       for(ll i=0;i<n;i++){
+          ll a,c;
+          cin>>a>>c;
+          // pushing end time, start time
+          v.push_back({c,a});
+       }
 }
