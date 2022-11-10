482c4a1e68f3566db987879913deb3b280583ebc
kerby <lovincekerby99@gmail.com>
2022-11-10 13:33:53 -0300

    add

--- Topicos_Especiais-XVI/RestCustomer.cpp
+++ Topicos_Especiais-XVI/RestCustomer.cpp
@@ -11,3 +11,16 @@ int main() {
 		times.push_back({start, 1});
 		times.push_back({end, -1});
 	}
+
+
+    sort(times.begin(), times.end());
+
+	int curr_ppl = 0;
+	int max_ppl = 0;
+	for (const pair<int, int>& t : times) {
+		curr_ppl += t.second;
+		max_ppl = max(max_ppl, curr_ppl);
+	}
+
+	cout << max_ppl << endl;
+}
