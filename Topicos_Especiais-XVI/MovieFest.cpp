a2f399ac64a08b98c7cca6fb419e049296bc6b31
kerby <lovincekerby99@gmail.com>
2022-11-10 13:40:32 -0300

    add

--- Topicos_Especiais-XVI/MovieFest.cpp
+++ Topicos_Especiais-XVI/MovieFest.cpp
@@ -1,3 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
+
+int main(){
+
+    ll T=1;
+    //cin>>T;
+    while(T--){
+       vector<pair<ll,ll>> v;
+       ll n;
+}
