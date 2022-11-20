e9b3a73d74233a874895344b707b3488a148cbc2
kerby <lovincekerby99@gmail.com>
2022-11-20 17:20:27 -0300

    commit

--- Linguagens/funcao_primeiro.hs
+++ Linguagens/funcao_primeiro.hs
@@ -1,2 +1,4 @@
 -- Defina uma função chamada primeiros que seleciona todos os elementos de uma lista não
 -- vazia, exceto o último, usando as funções do prelúdio.
+
+primeiros lista = reverse (tail (reverse lista))
