8f3c91b4975070d32163ddfc6abfb0564f4c4f78
kerby <lovincekerby99@gmail.com>
2022-11-20 16:27:10 -0300

    commit

--- Linguagens/Palindrome.hs
+++ Linguagens/Palindrome.hs
@@ -1,3 +1,7 @@
+-- Escreva uma função que recebe uma String e testa se ela representa uma palíndromo,
+-- retornando um Bool. Dica: para verificar se uma frase é palíndromo basta verificar se ela é
+-- igual à sua reversa.
+
 module Main (main) where
 main :: IO ()
 main = do putStrLn "Digite uma frase:"
