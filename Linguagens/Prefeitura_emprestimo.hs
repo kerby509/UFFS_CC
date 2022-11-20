6f426276bb2e0060730365ed0cb597fd942fd9d2
kerby <lovincekerby99@gmail.com>
2022-11-20 16:29:20 -0300

    commit

---
+++ Linguagens/Prefeitura_emprestimo.hs
@@ -0,0 +1,4 @@
+-- A prefeitura de Chapecó abriu uma linha de crédito para os funcionários estatutários. O
+-- valor máximo da prestação não poderá ultrapassar 30% do salário bruto. Fazer uma função
+-- que permita entrar com o salário bruto e o valor da prestação, e que retorne um booleano
+-- indicando se o funcionário pode ou não receber o empréstimo.
