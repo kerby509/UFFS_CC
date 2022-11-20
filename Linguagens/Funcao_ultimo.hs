6964374be778e6692b41897e3185895c59537d7a
kerby <lovincekerby99@gmail.com>
2022-11-20 17:18:40 -0300

    commit

--- Linguagens/Funcao_ultimo.hs
+++ Linguagens/Funcao_ultimo.hs
@@ -1,2 +1,6 @@
-Defina uma função chamada ultimo que seleciona o último elemento de uma lista não
-vazia, usando as funções do prelúdio.
+-- Defina uma função chamada ultimo que seleciona o último elemento de uma lista não
+-- vazia, usando as funções do prelúdio.
+
+ultimo lista = head (reverse lista)
+ultimo’ lista = lista !! (length lista - 1)
+ultimo’’ lista = head (drop (length lista - 1) lista)
