8196a0a52d7d44e60a256da6a82f10aba85c491c
kerby <lovincekerby99@gmail.com>
2022-11-21 15:53:52 -0300

    commit

---
+++ Linguagens/Preco_retrato.hs
@@ -0,0 +1,6 @@
+-- 4. O estúdio fotográfico Boa Imagem cobra de seus clientes por retratos antigos baseando-se
+-- no número de indivíduos incluídos no retrato. As tarifas constam da tabela seguinte.
+-- Retratos antigos tirados aos sábados ou aos domingos custam 20% a mais do que o preço
+-- base. Defina uma função precoRetrato do tipo Integer -> String -> Double que recebe como
+-- argumentos o número de pessoas no retrato e o dia da semana agendado, e calcula o custo do
+-- retrato. Exemplos:
