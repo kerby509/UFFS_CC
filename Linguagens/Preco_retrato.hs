0ef75ef3e6d2c4451a33eb4b49bb0728e0c10c95
kerby <lovincekerby99@gmail.com>
2022-11-21 15:54:30 -0300

    commit

--- Linguagens/Preco_retrato.hs
+++ Linguagens/Preco_retrato.hs
@@ -4,3 +4,16 @@
 -- base. Defina uma função precoRetrato do tipo Integer -> String -> Double que recebe como
 -- argumentos o número de pessoas no retrato e o dia da semana agendado, e calcula o custo do
 -- retrato. Exemplos:
+
+precoRetrato :: Integer -> String -> Float
+precoRetrato individuos diaSemana = precoBase + taxaExtra * precoBase
+where
+precoBase | individuos == 1 = 100
+| individuos == 2 = 130
+| individuos == 3 = 150
+| individuos == 4 = 165
+| individuos == 5 = 175
+| individuos == 6 = 180
+| otherwise = 185
+taxaExtra | diaSemana == "sábado" || diaSemana == "domingo" = 0.2
+| otherwise = 0
